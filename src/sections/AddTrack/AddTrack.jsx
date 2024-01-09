0f6c5df406d4e71bdf07a738545c7c0a47f24ab1
MatiasPViecho <mpviecho@gmail.com>
2024-01-08 23:54:33 -0300

    feat: add react-hook-form

--- src/sections/AddTrack/AddTrack.jsx
+++ src/sections/AddTrack/AddTrack.jsx
@@ -1,9 +1,29 @@
+'use client';
 import { TextInput } from '@/src/components/Inputs/Text/TextInput';
+import { useForm } from 'react-hook-form';
+
 export const AddTrack = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log('data:', data);
+  };
+  //TODO add functionality to use register via input
   return (
     <div className="text-slate-100 flex flex-col gap-4">
       <h1 className="text-5xl flex justify-center">Agregar Ducha 🚿</h1>
-      <TextInput />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextInput />
+        <input
+          defaultValue="test"
+          {...register('example')}
+        />
+        <button>CLICK</button>
+      </form>
     </div>
   );
 };
